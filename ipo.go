7b044038bb93e382d9bab36e7f787390c59f8b56
Harsh Goel <harshgoel05@gmail.com>
2024-09-27 18:14:56 +0530

    fix: ipo data

---
+++ ipo.go
@@ -0,0 +1,85 @@
+package main
+
+import "time"
+
+type IPO struct {
+	ID                  string     `json:"id"`                  // Unique ID of the IPO
+	Status              string     `json:"status"`              // Current status of the IPO (e.g., open, closed)
+	Slug                string     `json:"slug"`                // URL-friendly identifier
+	Name                string     `json:"name"`                // Name of the company issuing the IPO
+	Type                string     `json:"type"`                // Type of IPO (e.g., SME)
+	StartDate           time.Time  `json:"startDate"`           // Date when the IPO starts
+	EndDate             time.Time  `json:"endDate"`             // Date when the IPO ends
+	IsDateFinal         bool       `json:"isDateFinal"`         // Indicates if the IPO dates are final
+	ListingDate         time.Time  `json:"listingDate"`         // Date when the IPO will be listed on the stock exchange
+	PriceRange          PriceRange `json:"priceRange"`          // Price range for the IPO shares
+	MinQty              int        `json:"minQty"`              // Minimum quantity of shares to buy
+	SizePerLot          int        `json:"sizePerLot"`          // Number of shares per lot
+	IssueSize           string     `json:"issueSize"`           // Total size of the IPO issue
+	GmpDetails          GmpDetails `json:"gmpDetails"`          // Details about the grey market premium
+	ApplyRecommendation *bool      `json:"applyRecommendation"` // Recommendation for applying to the IPO (true - recommended, false - not recommended, null - no recommendation)
+	LastUpdatedAt       time.Time  `json:"lastUpdatedAt"`       // Date when the IPO data was last updated
+	CreatedAt           time.Time  `json:"createdAt"`           // Date when the IPO data was created
+}
+
+type IPODetail struct {
+	ID                  string     `json:"id"`                  // Unique ID of the IPO
+	Status              string     `json:"status"`              // Current status of the IPO (e.g., open, closed)
+	Slug                string     `json:"slug"`                // URL-friendly identifier
+	InfoURL             string     `json:"infoUrl"`             // Link to more information about the IPO
+	Name                string     `json:"name"`                // Name of the company issuing the IPO
+	CompanyDescription  string     `json:"companyDescription"`  // Description of the company
+	Symbol              string     `json:"symbol"`              // Stock symbol
+	Type                string     `json:"type"`                // Type of IPO (e.g., SME)
+	StartDate           time.Time  `json:"startDate"`           // Date when the IPO starts
+	EndDate             time.Time  `json:"endDate"`             // Date when the IPO ends
+	IsDateFinal         bool       `json:"isDateFinal"`         // Indicates if the IPO dates are final
+	ListingDate         time.Time  `json:"listingDate"`         // Date when the IPO will be listed on the stock exchange
+	PriceRange          PriceRange `json:"priceRange"`          // Price range for the IPO shares
+	MinQty              int        `json:"minQty"`              // Minimum quantity of shares to buy
+	SizePerLot          int        `json:"sizePerLot"`          // Number of shares per lot
+	Logo                string     `json:"logo"`                // URL to the logo image of the company
+	IssueSize           string     `json:"issueSize"`           // Total size of the IPO issue
+	ProspectusURL       string     `json:"prospectusUrl"`       // URL to download the IPO prospectus
+	Schedule            []Event    `json:"schedule"`            // List of events and their respective dates
+	GmpDetails          GmpDetails `json:"gmpDetails"`          // Details about the grey market premium
+	ApplyRecommendation *bool      `json:"applyRecommendation"` // Recommendation for applying to the IPO (true - recommended, false - not recommended, null - no recommendation)
+	LastUpdatedAt       time.Time  `json:"lastUpdatedAt"`       // Date when the IPO data was last updated
+	CreatedAt           time.Time  `json:"createdAt"`           // Date when the IPO data was created
+	Quota               Quota      `json:"quota"`               // Quota details for the IPO
+}
+
+type PriceRange struct {
+	Min float64 `json:"min"` // Minimum price in the range
+	Max float64 `json:"max"` // Maximum price in the range
+}
+
+type Event struct {
+	Event      string    `json:"event"`      // Description of the event (e.g., issue_open, listing_date)
+	Date       time.Time `json:"date"`       // Date when the event occurs
+	EVentTitle string    `json:"eventTitle"` // Title of the event
+}
+
+type GmpDetails struct {
+	GmpTimeline    []GmpTimeline `json:"gmpTimeline"`    // Description of the event (e.g., issue_open, listing_date)
+	LatestGmpPrice float64       `json:"latestGmpPrice"` // Date when the event occurs
+	IsGmpActivated bool          `json:"isGmpActivated"` // Title of the event
+}
+type GmpTimeline struct {
+	Event string    `json:"event"` // Description of the event (e.g., issue_open, listing_date)
+	Date  time.Time `json:"date"`  // Date when the event occurs
+}
+
+type Quota struct {
+	RetailQuota QuotaApplication `json:"retailQuota"` // Retail quota for the IPO
+	QibQuota    QuotaApplication `json:"qibQuota"`    // Qualified institutional buyer (QIB) quota for the IPO
+	SHNIQuota   QuotaApplication `json:"shniQuota"`   // Small high net-worth individual (SHNI) quota for the IPO
+	BHNIQuota   QuotaApplication `json:"bHNIQuota"`   // Big high net-worth individual (BHNI) quota for the IPO
+}
+
+type QuotaApplication struct {
+	OfferedShares            int `json:"offeredShares"`            // Number of shares offered in the IPO
+	AppliedShares            int `json:"appliedShares"`            // Number of shares applied for in the IPO
+	MinLotSizePerApplication int `json:"minLotSizePerApplication"` // Minimum lot size per application
+	MaxLotSizePerApplication int `json:"maxLotSizePerApplication"` // Maximum lot size per application
+}
---
+++ server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func readIpoJson() []IPODetail {
+	var ipoDetails []IPODetail
+	jsonFile, err := os.Open("ipo_data.json")
+	if err != nil {
+		log.Fatalf("Failed to open file: %s", err)
+	}
+	defer jsonFile.Close()
+
+	byteValue, _ := io.ReadAll(jsonFile)
+
+	if err := json.Unmarshal(byteValue, &ipoDetails); err != nil {
+		log.Fatalf("Error unmarshaling JSON: %s", err)
+	}
+	return ipoDetails
+
+}
+
+// getAlbums responds with the list of all albums as JSON.
+func getIpoCalendar(c *gin.Context) {
+
+	ipoDetails := readIpoJson()
+	c.IndentedJSON(http.StatusOK, ipoDetails)
+}
+
+func findIpoBySlug(ipoDetails []IPODetail, slug string) (*IPODetail, bool) {
+	for _, ipoDetail := range ipoDetails {
+		if ipoDetail.Slug == slug {
+			return &ipoDetail, true
+		}
+	}
+	return nil, false
+}
+
+func getIpoDetails(c *gin.Context) {
+	slug := c.Param("slug")
+	ipoDetail, found := findIpoBySlug(readIpoJson(), slug)
+	if !found {
+		c.JSON(http.StatusNotFound, gin.H{"error": "IPO not found"})
+	} else {
+		c.IndentedJSON(http.StatusOK, ipoDetail)
+	}
+}
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/calendar", getIpoCalendar)
+	router.GET("/details/:slug", getIpoDetails)
+	router.Run("localhost:8080")
+}
