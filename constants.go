40e1b6e38f0e3f42bf564309ca1bd34d766c74db
Harsh Goel <harshgoel05@gmail.com>
2024-10-01 13:14:09 +0530

    fix: url

--- constants.go
+++ constants.go
@@ -1,6 +1,6 @@
 package main
 
-var BASE_URL = "http://127.0.0.1:8000"
+// var BASE_URL = "http://127.0.0.1:8000"
 
-// var BASE_URL = "https://crawl.theipolist.com"
+var BASE_URL = "https://crawl.theipolist.in"
 var CALENDAR_API = "/calendar"
